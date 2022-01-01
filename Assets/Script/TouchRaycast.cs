890315349f5858009aa242f428edfdac9088a9c7
neptunine <nep4neptune@gmail.com>
2022-01-01 20:07:15 +0800

    A

---
+++ Assets/Script/TouchRaycast.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TouchRaycast : MonoBehaviour
+{
+    public int touchCount = 0;
+
+    public Vector3[]
+        touchedPositions;
+
+    public GameObject[]
+        touchedObjects;
+
+    void Update()
+    {
+        touchCount = Input.touchCount;
+
+        touchedPositions = new Vector3[touchCount];
+        touchedObjects = new GameObject[touchCount];
+
+        for (int i = 0; i < touchCount; i++)
+        {
+            touchedPositions[i] = Camera.main.ScreenToWorldPoint(Input.touches[i].position);
+            Debug.DrawLine(Vector3.zero, touchedPositions[i], Color.red);
+
+            RaycastHit2D hitInformation = Physics2D.Raycast(new Vector2(touchedPositions[i].x, touchedPositions[i].y), Camera.main.transform.forward);
+
+            if (hitInformation.collider != null)
+            {
+                    touchedObjects[i] = hitInformation.transform.gameObject;
+            }
+        }
+    }
+}
