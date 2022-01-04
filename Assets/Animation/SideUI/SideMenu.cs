8de1022b8b0b26882e103d88a3fa375ee0280a1a
neptunine <nep4neptune@gmail.com>
2022-01-04 17:58:07 +0800

    UI

---
+++ Assets/Animation/SideUI/SideMenu.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace UI
+{
+    public class SideMenu : MonoBehaviour
+    {
+        void Start()
+        {
+            gameObject.SetActive(false);
+        }
+
+        public void Hide()
+        {
+            gameObject.SetActive(false);
+        }
+    }
+}
