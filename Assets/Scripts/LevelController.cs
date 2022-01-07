98cae92c91f7f57ea6b3d03751a63fba4aa7ba3b
neptunine <nep4neptune@gmail.com>
2022-01-08 04:20:58 +0800

    added level logic

--- Assets/Scripts/LevelController.cs
+++ Assets/Scripts/LevelController.cs
@@ -20,30 +20,36 @@ namespace Game
         public float
             percent;
 
-        public Color
-            initcolor,
-            selectColor;
+        public float
+            lerpSpeed = 4f;
 
         [SerializeField]
         GameObject
-            charDisplay;
+            charDisplay,
+            charInput,
+            selector;
 
         private Transform
-            charParent;
+            selectedInput;
 
-        [SerializeField]
-        Text text;
+        private Vector3
+            initalPos,
+            targetPos;
 
         private string[]
             words;
 
         private char
-            hideChar = '_',
-            spacChar = ' ';
+            hideChar = ' ';
+
+        private char[]
+            inputChars = new char[18];
 
         private string
-            targetWord,
-            displayWord;
+            targetWord;
+
+        public int[]
+            missing;
 
         private GameObject
             selected;
@@ -51,44 +57,67 @@ namespace Game
         public int
             score;
 
+        private char[]
+            abc;
+
         private void Awake()
         {
             words = wordFile.text.Split("\n"[0]);
+            abc = "ABCDEFGHIJKLMNOPQRSTUVWXYZABCDEFGHIJKLMNOPQRSTUVWXYZ".ToCharArray();
+            for (int i = 0; i < abc.Length; i++)
+            {
+                char tmp = abc[i];
+                int r = Random.Range(i, abc.Length);
+                abc[i] = abc[r];
+                abc[r] = tmp;
+            }
         }
 
         private void Start()
         {
-            charParent = charDisplay.transform.parent;
-            initcolor = charDisplay.GetComponent<Image>().color;
             Initialize();
         }
 
         private void Update()
         {
+            if (selected)
+            {
+                selector.transform.position = new Vector3(Mathf.Lerp(selector.transform.position.x, selected.transform.position.x, Time.deltaTime * lerpSpeed), Mathf.Lerp(selector.transform.position.y, selected.transform.position.y, Time.deltaTime * lerpSpeed), selector.transform.position.z);
+            }
+            if (selectedInput)
+            {
+                selectedInput.position = new Vector3(Mathf.Lerp(selectedInput.position.x, targetPos.x, Time.deltaTime * lerpSpeed), Mathf.Lerp(selectedInput.position.y, targetPos.y, Time.deltaTime * lerpSpeed), selectedInput.position.z);
+            }
 
         }
 
         public void Initialize()
         {
-            targetWord = words[Random.Range(0, words.Length)].Trim();
+            targetWord = words[Random.Range(0, words.Length)].Trim().ToUpper();
             Debug.Log($"[{this.name}] picked '{targetWord}'");
 
             char[] wordChar = targetWord.ToCharArray();
-            for (int i = 0; i < wordChar.Length * percent;)
+            int len = Mathf.RoundToInt(wordChar.Length * percent);
+            missing = new int[len];
+            for (int i = 0; i < len;)
             {
                 int index = Random.Range(0, wordChar.Length - 1);
                 if (wordChar[index] != hideChar)
                 {
+                    inputChars[index] = wordChar[index];
                     wordChar[index] = hideChar;
                     i++;
                 }
             }
-            //char[] tmpChar2 = new char[tmpChar.Length * 2 - 1];
-            //for (int i = 0; i < tmpChar2.Length; i++)
-            //{
-            //    if (i % 2 == 0) tmpChar2[i] = tmpChar[i / 2];
-            //    else tmpChar2[i] = spacChar;
-            //}
+            for (int i = 0, j = 0; i < wordChar.Length; i++)
+            {
+                if (wordChar[i] == hideChar)
+                {
+                    missing[j] = i;
+                    j++;
+                }
+            }
+
 
             for (int i = 0; i < wordChar.Length; i++)
             {
@@ -98,6 +127,8 @@ namespace Game
                 if (wordChar[i] == hideChar)
                 {
                     Button button = charObject.GetComponentInChildren<Button>();
+                    if (selected == null)
+                        SelectChar(button);
                     button.interactable = true;
                     Button self = button;
                     button.onClick.AddListener(() => SelectChar(self));
@@ -105,9 +136,32 @@ namespace Game
                 charObject.SetActive(true);
             }
 
-            displayWord = new string(wordChar);
+            for (int i = 0; i < inputChars.Length; i++)
+            {
+                if (inputChars[i] == 0)
+                {
+                    inputChars[i] = abc[i];
+                }
+            }
+            for (int i = 0; i < inputChars.Length; i++)
+            {
+                char tmp = inputChars[i];
+                int r = Random.Range(i, inputChars.Length);
+                inputChars[i] = inputChars[r];
+                inputChars[r] = tmp;
+            }
+            for (int i = 0; i < inputChars.Length; i++)
+            {
+                GameObject charObject = Instantiate(charInput, charInput.transform.parent);
+                charObject.name = i.ToString();
+                charObject.GetComponentInChildren<Text>().text = inputChars[i].ToString();
+                Button button = charObject.GetComponentInChildren<Button>();
+                button.interactable = true;
+                Button self = button;
+                button.onClick.AddListener(() => InputChar(self));
+                charObject.SetActive(true);
+            }
 
-            text.text = displayWord;
         }
 
         public void Terminate()
@@ -117,13 +171,67 @@ namespace Game
 
         public void SelectChar(Button clicked)
         {
-            if (selected != null)
+            selected = clicked.gameObject;
+
+        }
+
+        public void InputChar(Button clicked)
+        {
+            selectedInput = clicked.transform;
+            initalPos = selectedInput.position;
+            targetPos = selector.transform.position;
+
+            char inchar = selectedInput.GetComponentInChildren<Text>().text.ToCharArray()[0];
+            int no, k;
+            int.TryParse(selected.name, out no);
+            for (int i = 0; i < missing.Length; i++)
             {
-                selected.GetComponent<Image>().color = initcolor;
+                if (missing[i] == no)
+                {
+                    k = missing[i];
+
+                    if (inchar != targetWord[k])
+                    {
+                        StartCoroutine(IncorrectInput());
+                        return;
+                    }
+
+                    missing[i] = -1;
+
+                    if (i < missing.Length - 1)
+                    {
+                        no = missing[i + 1];
+                        break;
+                    }
+                    else
+                    {
+                        no = -1;
+                        for (int j = 0; j < missing.Length; j++)
+                        {
+                            if (missing[j] != -1) no = missing[j];
+                        }
+                        break;
+                    }
+                }
             }
 
-            selected = clicked.gameObject;
-            selected.GetComponent<Image>().color = selectColor;
+            if (no != -1)
+            {
+                Transform found = charDisplay.transform.parent.Find(no.ToString());
+                if (found != null)
+                    SelectChar(found.GetComponent<Button>());
+            }
+            else
+            {
+                Debug.Log("no more");
+            }
+        }
+
+        private IEnumerator IncorrectInput()
+        {
+            yield return new WaitForSeconds(1);
+
+            targetPos = initalPos;
         }
     }
 }
