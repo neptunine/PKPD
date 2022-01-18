21c663ee2d710a03f3c8ed68203d20fae035b6ec
neptunine <nep4neptune@gmail.com>
2022-01-19 01:10:06 +0800

    ah

--- Assets/Scripts/LevelController.cs
+++ Assets/Scripts/LevelController.cs
@@ -41,16 +41,10 @@ namespace Game
             targetWord;
 
         private char[]
-            abc;
-        
-        private char
-            hideChar = ' ';
-        
+           expectChar;
+
         private char[]
-            inputChars = new char[18];
-        
-        private int[]
-            missing;
+            abc = "ABCDEFGHIJKLMNOPQRSTUVWXYZABCDEFGHIJKLMNOPQRSTUVWXYZ".ToCharArray();
 
         private Transform
             selected,
@@ -65,12 +59,12 @@ namespace Game
 
         private void Awake()
         {
-            
+
         }
 
         private void Start()
         {
-            
+
         }
 
         private void Update()
@@ -80,24 +74,24 @@ namespace Game
 
         public void Initialize()
         {
-            abc = "ABCDEFGHIJKLMNOPQRSTUVWXYZABCDEFGHIJKLMNOPQRSTUVWXYZ".ToCharArray();
             string[] words = wordFile.text.Split("\n"[0]);
             targetWord = words[Random.Range(0, words.Length)].Trim().ToUpper();
             Debug.Log($"[{this.name}] picked '{targetWord}'");
 
-            char[] wordChar = targetWord.ToCharArray();
+            char[] disChars = targetWord.ToCharArray();
+            char[] inChars = new char[18];
+            expectChar = new char[disChars.Length];
             percent = Mathf.Clamp01(percent);
-            int len = Mathf.RoundToInt(wordChar.Length * percent);
-            missing = new int[len];
-            if (len < wordChar.Length)
+            int len = Mathf.RoundToInt(disChars.Length * percent);
+            if (len < disChars.Length)
             {
                 for (int i = 0; i < len;)
                 {
-                    int index = Random.Range(0, wordChar.Length - 1);
-                    if (wordChar[index] != hideChar)
+                    int index = Random.Range(0, disChars.Length - 1);
+                    if (disChars[index] != '\0')
                     {
-                        inputChars[index] = wordChar[index];
-                        wordChar[index] = hideChar;
+                        inChars[index] = expectChar[index] = disChars[index];
+                        disChars[index] = '\0';
                         i++;
                     }
                 }
@@ -106,21 +100,13 @@ namespace Game
             {
                 for (int i = 0; i < len; i++)
                 {
-                    inputChars[i] = wordChar[i];
-                    wordChar[i] = hideChar;
-                }
-            }
-            for (int i = 0, j = 0; i < wordChar.Length; i++)
-            {
-                if (wordChar[i] == hideChar)
-                {
-                    missing[j] = i;
-                    j++;
+                    inChars[i] = expectChar[i] = disChars[i];
+                    disChars[i] = '\0';
                 }
             }
 
             LayoutGroup displayLayout = charDisplay.GetComponentInChildren<LayoutGroup>();
-            for (int i = 0; i < wordChar.Length; i++)
+            for (int i = 0; i < disChars.Length; i++)
             {
                 GameObject charObject = Instantiate(charPrefab, charDisplay.transform);
                 charObject.GetComponent<RectTransform>().anchoredPosition3D = Vector3.zero;
@@ -130,8 +116,8 @@ namespace Game
                 sf.anchor.name = i.ToString();
                 sf.freezePosition.X = false;
                 sf.freezePosition.Y = sf.freezePosition.Z = true;
-                charObject.GetComponentInChildren<Text>().text = wordChar[i].ToString();
-                if (wordChar[i] == hideChar)
+                charObject.GetComponentInChildren<Text>().text = disChars[i].ToString();
+                if (disChars[i] == '\0')
                 {
                     Button button = charObject.GetComponentInChildren<Button>();
                     if (selected == null)
@@ -151,23 +137,23 @@ namespace Game
                 abc[i] = abc[r];
                 abc[r] = tmp;
             }
-            for (int i = 0; i < inputChars.Length; i++)
+            for (int i = 0; i < inChars.Length; i++)
             {
-                if (inputChars[i] == 0)
+                if (inChars[i] == 0)
                 {
-                    inputChars[i] = abc[i];
+                    inChars[i] = abc[i];
                 }
             }
-            for (int i = 0; i < inputChars.Length; i++)
+            for (int i = 0; i < inChars.Length; i++)
             {
-                char tmp = inputChars[i];
-                int r = Random.Range(i, inputChars.Length);
-                inputChars[i] = inputChars[r];
-                inputChars[r] = tmp;
+                char tmp = inChars[i];
+                int r = Random.Range(i, inChars.Length);
+                inChars[i] = inChars[r];
+                inChars[r] = tmp;
             }
 
             LayoutGroup inputLayout = charInput.GetComponentInChildren<LayoutGroup>();
-            for (int i = 0; i < inputChars.Length; i++)
+            for (int i = 0; i < inChars.Length; i++)
             {
                 GameObject charObject = Instantiate(charPrefab, charInput.transform);
                 charObject.GetComponent<RectTransform>().anchoredPosition3D = Vector3.zero;
@@ -177,7 +163,7 @@ namespace Game
                 sf.anchor.name = i.ToString();
                 sf.freezePosition.X = sf.freezePosition.Y = false;
                 sf.freezePosition.Z = true;
-                charObject.GetComponentInChildren<Text>().text = inputChars[i].ToString();
+                charObject.GetComponentInChildren<Text>().text = inChars[i].ToString();
                 Button button = charObject.GetComponentInChildren<Button>();
                 button.interactable = true;
                 Button self = button;
@@ -213,39 +199,42 @@ namespace Game
             selectedInput.GetComponent<Button>().interactable = false;
 
             char inchar = selectedInput.GetComponentInChildren<Text>().text.ToCharArray()[0];
-            int.TryParse(selected.name, out int no);
-            for (int i = 0; i < missing.Length; i++)
+            int.TryParse(selected.name, out int i);
+
+            if (inchar != expectChar[i])
             {
-                if (missing[i] == no)
+                StartCoroutine(OnIncorrectInput());
+                return;
+            }
+            expectChar[i] = '\0';
+
+            int next = -1;
+            if (i < expectChar.Length - 1)
+            {
+                for (int j = i; j < expectChar.Length; j++)
                 {
-                    if (inchar != targetWord[missing[i]])
+                    if (expectChar[j] != '\0')
                     {
-                        StartCoroutine(OnIncorrectInput());
-                        return;
-                    }
-
-                    missing[i] = -1;
-
-                    if (i < missing.Length - 1)
-                    {
-                        no = missing[i + 1];
+                        next = j;
                         break;
                     }
-                    else
+                }
+            }
+            else
+            {
+                for (int j = 0; j < i; j++)
+                {
+                    if (expectChar[j] != '\0')
                     {
-                        no = -1;
-                        for (int j = 0; j < missing.Length; j++)
-                        {
-                            if (missing[j] != -1) no = missing[j];
-                        }
+                        next = j;
                         break;
                     }
                 }
             }
 
-            if (no != -1)
+            if (next != -1)
             {
-                StartCoroutine(OnCorrectInput(no));
+                StartCoroutine(OnCorrectInput(next));
             }
             else
             {
