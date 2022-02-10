f5edc908cff023e6cc5ea0f76c055af32d849eb1
neptunine <nep4neptune@gmail.com>
2022-02-11 01:41:53 +0800

    added canvas scaler

---
+++ Assets/Scripts/UI/CanvasPP.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using UnityEngine.U2D;
+using UnityEngine.UI;
+
+namespace UI
+{
+    [ExecuteInEditMode]
+    [RequireComponent(typeof(CanvasScaler))]
+    public class CanvasPP : MonoBehaviour
+    {
+        private PixelPerfectCamera
+            _pixelPerfectCamera;
+
+        private CanvasScaler
+            _canvasScaler;
+
+        private void Start()
+        {
+            _pixelPerfectCamera = Camera.main.GetComponent<PixelPerfectCamera>();
+            _canvasScaler = GetComponent<CanvasScaler>();
+        }
+        private void Update()
+        {
+            if (_pixelPerfectCamera && _pixelPerfectCamera.enabled)
+            {
+#if UNITY_EDITOR
+                if (Application.isPlaying)
+                    _canvasScaler.scaleFactor = _pixelPerfectCamera.pixelRatio;
+                else
+                    if (_pixelPerfectCamera.runInEditMode)
+                        _canvasScaler.scaleFactor = _pixelPerfectCamera.pixelRatio;
+#else
+                _canvasScaler.scaleFactor = _pixelPerfectCamera.pixelRatio;
+#endif
+            }
+        }
+    }
+}
