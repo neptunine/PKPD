b4b24a385b8ae3d85f73bbf6e9fb51023b146781
tommyboyy5 <tommy2001516@yahoo.com.hk>
2022-02-14 21:56:03 +0800

    option try

---
+++ Assets/Scripts/test.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class test : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        PlayerPrefs.SetInt("test2", 666);
+        var a2 = PlayerPrefs.GetInt("test2");
+        Debug.Log(a2);
+    }
+
+    
+}
