25670f0328601152de00db7b6bd3301470b4d9dd
tommyboyy5 <tommy2001516@yahoo.com.hk>
2022-02-17 00:33:20 +0800

    change

--- Assets/Scripts/UI/Credits.cs
+++ Assets/Scripts/UI/Credits.cs
@@ -1,18 +1,10 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
+using UnityEngine.UI;
 
 public class Credits : MonoBehaviour
 {
     // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+    public Text title;
+    public Text progress;
+    public Image fill;
 }
