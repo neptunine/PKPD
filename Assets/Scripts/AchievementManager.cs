dcb5012a9f922b04aa29a8f9c5c1d2d94ccdd774
tommyboyy5 <tommy2001516@yahoo.com.hk>
2022-03-03 01:30:04 +0800

    try
    trytry

---
+++ Assets/Scripts/AchievementManager.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AchievementManager : MonoBehaviour
+{
+    public GameObject achievementPrefab;
+    void Start()
+    {
+        CreateAchievement("General");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    public void CreateAchievement(string category)
+    {
+        GameObject achievement = (GameObject)Instantiate(achievementPrefab);
+
+        SetAchievementInfo(category, achievement);
+    }
+
+    public void SetAchievementInfo(string category,GameObject achievement)
+    {
+        achievement.transform.SetParent(GameObject.Find(category).transform);
+    }
+}
