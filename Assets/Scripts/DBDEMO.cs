4d4c526bc14d5e2dcc4a032d9022bb8f3661fe71
tommyboyy5 <tommy2001516@yahoo.com.hk>
2022-03-18 00:54:45 +0800

    DataBase
    DataBase

---
+++ Assets/Scripts/DBDEMO.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Mono.Data.Sqlite;
+using System.Data;
+
+public class DBDEMO : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        DisplayWords();
+    }
+
+    public void DisplayWords()
+    {
+        string dbName = "URI=file:WordsDB.db";
+
+        using (var connection = new SqliteConnection(dbName))
+        {
+            connection.Open();
+
+            using(var command = connection.CreateCommand())
+            {
+                command.CommandText = "SELECT * FROM Words";
+
+                using(IDataReader reader = command.ExecuteReader())
+                {
+                    while (reader.Read())
+                    {
+                        Debug.Log("Words:" + reader["Name"]);
+                    }
+                    reader.Close();
+                }
+            }
+            connection.Close();
+        }
+        
+
+    }
+
+    
+}
